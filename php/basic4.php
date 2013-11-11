5c6b109e7fc88c6163fafdb923b2521ce571e478
fffrozen <rmacapagal@village88.com>
2013-11-11 18:28:57 +0800

    organized the javascript folder

--- php/basic4.php
+++ php/basic4.php
@@ -8,27 +8,33 @@
 	<?php
 		$numbers = array(135, 2.4, 2.67, 1.23, 332, 2, 1.02);
 		
-		$lower_number = $numbers[0];
-		$higher_number = $numbers[0];
-		
-		for($i = 0; $i < count($numbers); $i++)
+		function get_max_and_min()
 		{
-			$current_number = $numbers[$i];
-			if($current_number <= $lower_number)
+			$lower_number = array[0];
+			$higher_number = array[0];
+			
+			for($i = 0; $i < count(array); $i++)
 			{
-				$lower_number = $current_number;
+				$current_number = $numbers[$i];
+				if($current_number <= $lower_number)
+				{
+					$lower_number = $current_number;
+				}
+				else if($current_number >= $higher_number)
+				{
+					$higher_number = $current_number;
+				}
+				else
+				{
+					
+				}
 			}
-			else if($current_number >= $higher_number)
-			{
-				$higher_number = $current_number;
-			}
-			else
-			{
-				
-			}
+			echo "array('max' => " . $higher_number . ", 'min' => " . $lower_number;
+			//echo "min: " . $lower_number . "<br />max: " . $higher_number;
 		}
 		
-		echo "min: " . $lower_number . "<br />max: " . $higher_number;
+		$output = get_max_and_min($numbers);
+		var_dump($output);
 	?>
 </body>
 </html>
