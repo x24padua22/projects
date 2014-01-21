b92d5a358f60ceb5a6264b29a453f212fecdd1b5
fffrozen <rmacapagal@village88.com>
2014-01-21 11:43:27 +0800

    finished the chatroom assignment

--- nodes/Express_IO/chatroom/routes/index.js
+++ nodes/Express_IO/chatroom/routes/index.js
@@ -1,19 +1,21 @@
 module.exports = function Route(app){
   var all_users = [];
-  var counter = 0;
 
   app.get("/", function(req, res){
   	res.render("index");
   });
 
   app.io.route("got_a_new_user", function(req){
-  	counter++;
-    req.io.broadcast("new_user", {user_no: counter, new_user_name: req.data.name});
-  	all_users.push({user_no: counter, name: req.data.name});
+    req.io.broadcast("new_user", {new_user_name: req.data.name});
+  	all_users.push(req.data.name);
   	req.io.emit("existing_users", {users: all_users});
   });
 
   app.io.route("disconnect", function(req){
   	console.log("A user has disconnected", req.name);
   })
+
+  app.io.route("updated_message", function(req){
+    req.io.broadcast("message_update", {sender: req.data.sender, message: req.data.key});
+  });
 };
