8dd4b3c5d74e62ee7870db281486ad293912c590
fffrozen <rmacapagal@village88.com>
2014-02-04 10:56:07 +0800

    pushed the finished project

--- village88_times/village88_times/routes/index.js
+++ village88_times/village88_times/routes/index.js
@@ -3,31 +3,11 @@ module.exports = function Route(app, mysql, db){
 	var name = "";
 	var date = new Date();
 	var month = date.getMonth() + 1;
+	var day = date.getDate() - 1;
+	var yesterday = date.getFullYear()+"-"+month+"-"+day;
 
 	if(month < 10)
 		month = "0"+month;
-
-	function get_time(date)
-	{
-		var hour = date.getHours();
-		var minute = date.getMinutes();
-		var am_pm = "am";
-
-		if(hour >= 13)
-		{
-			hour -= 12;
-			am_pm = "pm";
-		}
-		else if(hour == 12)
-			am_pm = "pm"
-		else if(hour == 0)
-			hour = 12;
-
-		if(minute < 10)
-			minute = "0"+minute;
-
-		return hour+":"+minute+" "+am_pm;
-	}
 
 	app.get("/", function(req, res){
 		res.render("index");
@@ -41,6 +21,13 @@ module.exports = function Route(app, mysql, db){
 		res.render("profile");
 	});
 
+	app.io.route("get_photos", function(req){
+		get_users_photos = function(errors, results, fields){
+			req.io.emit("users_photo_url", {photo_url: results});
+		}
+		db.query("SELECT photo_url FROM users", get_users_photos);
+	})
+
 	app.io.route("check_is_logged_in", function(req){
 		for(var index in online_users)
 		{
@@ -53,36 +40,43 @@ module.exports = function Route(app, mysql, db){
 		get_user = function(errors, results, fields){
 			if(results[0])
 			{
-				online_users.push({user_id: results[0].id, user_name: results[0].first_name, session_id: req.session.id});
-				req.io.broadcast("new_user", {user_name: results[0].first_name, user_id: results[0].id});
+				online_users.push({user_id: results[0].id,
+								   user_name: results[0].first_name,
+								   session_id: req.session.id,
+								   photo_url: results[0].photo_url
+								});
+				req.io.broadcast("new_user", {user_name: results[0].first_name,
+											  user_id: results[0].id,
+											  photo_url: results[0].photo_url
+											});
 				req.io.emit("login_redirect", {url: "/wall"});
 			}
 			else
 				req.io.emit("login_failed", {message: "Invalid email/password"});
 		}
 		
-		var get_user_query = db.query("SELECT * FROM users "+ 
-									  "WHERE email='"+req.data.email+"'"+
-									  "AND password='"+req.data.password+"'", get_user);
+		db.query("SELECT * FROM users "+ 
+				 "WHERE email='"+req.data.email+"'"+
+				 "AND password='"+req.data.password+"'", get_user);
 	});
 
 	app.io.route("get_user_info", function(req, res){
 		for(var index in online_users)
 		{
 			if(online_users[index].session_id == req.session.id)
-				req.io.emit("user_info", {user_name: online_users[index].user_name, user_id: online_users[index].user_id, other_online_users: online_users});
+				req.io.emit("user_info", {user_name: online_users[index].user_name,
+										  user_id: online_users[index].user_id,
+										  photo_url: online_users[index].photo_url,
+										  all_online_users: online_users});
 		}
-	});
-
-	app.io.route("get_current_time", function(req){
-		var current_date = new Date();
-		req.io.emit("current_time", {time: get_time(current_date)});
 	});
 
 	app.io.route("check_last_clock_record", function(req){
 		get_last_record = function(errors, results, fields){
 			if(results[0].clock_in_time)
-				req.io.emit("clock_record_form_values", {clock_button_value: "Clock-out", description: results[0].description});
+				req.io.emit("clock_record_form_values", {clock_button_value: "Clock-out",
+														 description: results[0].description
+														});
 			else
 				req.io.emit("clock_record_form_values", {clock_button_value: "Clock-in"});
 		}
@@ -95,24 +89,13 @@ module.exports = function Route(app, mysql, db){
 			req.io.emit("clock_records", {clock_records: results});
 		}
 
-		day = date.getDate() - 1;
-		var yesterday = date.getFullYear()+"-"+month+"-"+day;
-
 		db.query("SELECT clock_records.description, clock_records.clock_in_time, clock_records.clock_out_time, users.first_name FROM clock_records JOIN users ON users.id = clock_records.user_id WHERE clock_records.clock_in_time >= '"+yesterday+"' OR clock_records.clock_out_time >= '"+yesterday+"' ORDER BY clock_records.id DESC", get_clock_records);
 	});
-
-	app.io.route("convert_date_time", function(req){
-		var date = new Date(req.data.date_time);
-		req.io.emit("converted_time", {time: get_time(date)});
-	})
 
 	app.io.route("get_user_clock_records", function(req){
 		get_user_clock_records = function(errors, results, fields){
 			req.io.emit("user_clock_records", {user_activities: results});
 		}
-
-		day = date.getDate() - 1;
-		var yesterday = date.getFullYear()+"-"+month+"-"+day;
 
 		db.query("SELECT * FROM clock_records WHERE user_id ='"+req.data.user_id+"' ORDER BY id DESC", get_user_clock_records);
 	})
@@ -123,27 +106,32 @@ module.exports = function Route(app, mysql, db){
 		else
 			clock_value = "clock_out_time";
 
-		var date_time = date.getFullYear()+"-"+month+"-"+date.getDate()+" "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
-
+		date = new Date();
+		date_time = date.getFullYear()+"-"+month+"-"+date.getDate()+" "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+		
 		var insert_clock_record = db.query("INSERT INTO clock_records (user_id, description, "+clock_value+", created_at) VALUES ('"
 			+req.data.user_id+"', '"+req.data.description+"', '"+date_time+"', '"+date_time+"')");
 
 		name = req.data.user_name;
-		app.io.broadcast("clock_record_success", {user_name: name, description: req.data.description, time: date_time, clock_value: clock_value});
+		app.io.broadcast("clock_record_success", {user_name: name,
+												  description: req.data.description,
+												  time: date_time,
+												  clock_value: clock_value});
 		req.io.emit("user_success", {clock_value: clock_value});
 	});
 
-	app.io.route("logout", function(req, res){
+	app.get("/logout", function(req, res){
 		for(var index in online_users)
 		{
 			if(req.session.id == online_users[index].session_id)
 			{
-				online_users.pop(online_users[index]);
-				//delete online_users[index];
+				user_id = online_users[index].user_id;
+				//online_users.pop(online_users[index]);
+				delete online_users[index];
 			}
 		}
 
-		req.io.broadcast("user_logged_out", {user_id: req.data.user_id});
-		req.io.emit("redirect_to_index", {url: "/"});
+		app.io.broadcast("user_logged_out", {user_id: user_id});
+		res.render("index");
 	});
 }
